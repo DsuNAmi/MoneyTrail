89e2e21b5feea0b2bce7d771d6b26ed64ad59fbd
Huracan- <453243067@qq.com>
2025-09-26 07:41:51 +0800

    fix the server's bug

--- backend/includes/server.hpp
+++ backend/includes/server.hpp
@@ -10,6 +10,8 @@
 #include <sstream>
 #include <fstream>
 #include <optional>
+#include <queue>
+#include <chrono>
 
 #include <boost/asio.hpp>
 #include <boost/beast.hpp>
@@ -31,6 +33,7 @@ class Session : public std::enable_shared_from_this<Session>{
         virtual ~Session();
 
         void start() {do_read();}
+        void stop();
 
         std::string get_client_name() const {return _clinet_name;}
 
@@ -38,59 +41,62 @@ class Session : public std::enable_shared_from_this<Session>{
     private:
         void do_read();
         void handle_read(boost::system::error_code ec);
+        void handle_request(net_string && request);
+
+        void do_write();
+        void handle_write(boost::system::error_code ec, std::size_t bytes);
+
         void do_close();
+        void clear_queue();
+
+
 
     private:
-        boost::asio::ip::tcp::socket _socket;
         boost::beast::tcp_stream _stream;
         boost::beast::flat_buffer _buffer;
-
-
-        std::optional<boost::beast::http::request_parser<boost::beast::http::string_body>> _parser;
-
+        
+        
+        
         std::string _clinet_name;
-
+        std::atomic<bool> _closing;
+        
+        std::optional<boost::beast::http::request_parser<boost::beast::http::string_body>> _parser;
 
         Server & _server;
         Logger & _logger;
-        
 
+        std::queue<std::shared_ptr<boost::beast::http::response<boost::beast::http::string_body>>> _response_queue;
+        std::atomic<bool> _writing;
 };
 
 
 class Server{
     public:
         Server(std::string host, unsigned short port, int thread_number, Logger & logger);
-
-        void start();
-
-        void public_hanlde_requet(
-            net_string && request,
-            boost::beast::tcp_stream & stream
-        ){
-            handle_request(std::move(request), stream);
-        }
-
         virtual ~Server();
     
         Server(const Server &) = delete;
         Server & operator=(const Server &) = delete;
+        
+        
+        void start();
+        void stop();
 
-    
-    
+    public:
+
+        void add_client(const std::shared_ptr<Session> & session);
+        void remove_client(const std::string & client_name);
+        void close_all_clients();
+
+        
+        
     private:
 
         void do_accept();
 
-        void handle_request(
-            net_string && request,
-            boost::beast::tcp_stream & stream
-        );
-
         boost::asio::ip::tcp::endpoint make_endpoint();
 
 
-        void stop();
 
     private:
         std::string _host;
@@ -100,9 +106,8 @@ class Server{
         boost::asio::ip::tcp::acceptor _acceptor;
 
 
-    private:
-        std::unordered_map<std::string, std::shared_ptr<Session>> _clinets;
-        Logger & _logger;
-        
 
+    private:
+        std::unordered_map<std::string, std::shared_ptr<Session>> _clients;
+        Logger & _logger;
 };
--- backend/server.cpp
+++ backend/server.cpp
@@ -4,20 +4,47 @@
 
 //Session
 Session::Session(boost::asio::ip::tcp::socket socket, Server & server, Logger & logger)
-: _socket(std::move(socket)),
-  _stream(_socket),
+: _stream(std::move(socket)),
   _server(server),
   _logger(logger),
-  _clinet_name(
-    _socket.remote_endpoint().address().to_string() + 
-    ":" +
-    std::to_string(_socket.remote_endpoint().port())
-  )
+  _closing(false), _writing(false)
 {
-    _logger.log_file(LogLevel::INFO, "Clinet " + _clinet_name + " Has been connected.");
+    try
+    {
+        auto & socket_ref = _stream.socket();
+        auto remote_endpoint = socket_ref.remote_endpoint();
+        _clinet_name = remote_endpoint.address().to_string() + 
+                        ":" + std::to_string(remote_endpoint.port());
+    }
+    catch(const std::exception& e)
+    {
+        _clinet_name = "unkonwn_client";
+        _logger.log_file(LogLevel::WARNING,"Could not get client endpoint: " + std::string(e.what()));
+    }
+    _logger.log_file(LogLevel::INFO, "Client " + _clinet_name + " has been connected.");
+    
 }
 
+Session::~Session(){
+    if(!_closing){
+        stop();
+    }
+    _logger.log_file(LogLevel::INFO, "Session for " + _clinet_name + " destroyed.");
+}
+
+void Session::stop(){
+    if(_closing.exchange(true)){
+        return; // closing, avoid to close again
+    }
+    _logger.log_file(LogLevel::INFO, "Closing session for " + _clinet_name);
+    do_close();
+}
+
+
+
 void Session::do_read(){
+    if(_closing) return;
+
     _parser.emplace();
     _parser->body_limit(1024 * 1024);
 
@@ -33,121 +60,45 @@ void Session::do_read(){
 
 void Session::handle_read(boost::system::error_code ec){
     if(ec == boost::beast::http::error::end_of_stream){
-        return do_close();
+        _logger.log_file(LogLevel::INFO, "Client " + _clinet_name + " closed connection gracefully.");
+        return stop();
     }
     if(ec){
-        _logger.log_file(LogLevel::ERROR, "Read Error: " + ec.message());
-        return;
+        if(ec != boost::asio::error::operation_aborted){
+            _logger.log_file(LogLevel::ERROR, "Read Error: " + ec.message());
+        }
+        return stop();
     }
 
-    _server.public_hanlde_requet(_parser->release(), _stream);
+    handle_request(_parser->release());
 
-
-
-    if(_parser->keep_alive()){
+    if(_parser->keep_alive() && !_closing){
         do_read();
     }else{
-        do_close();
-    }
-}
-
-void Session::do_close(){
-    boost::system::error_code ec;
-    _stream.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_send, ec);
-}
-
-
-
-// Server
-Server::Server(std::string host, unsigned short port, int thread_number, Logger & logger)
-: _host(std::move(host)), _port(port), _thread_number(thread_number),
-  _ioc(_thread_number), _acceptor(_ioc, make_endpoint()),
-  _logger(logger)
-{
-    _logger.log_file(LogLevel::INFO, "Server initiallized Successfully.");
-}
-
-Server::~Server(){
-    stop();
-    _logger.log_file(LogLevel::INFO, "Server stopped. All things cleaned up.");
-}
-
-
-
-void Server::stop(){
-    boost::system::error_code ec;
-    _acceptor.close(ec);
-    if(ec){
-        _logger.log_file(LogLevel::ERROR, "Error closing acceptor: " + ec.message());
-    }
-    _ioc.stop();
-}
-
-
-boost::asio::ip::tcp::endpoint Server::make_endpoint(){
-    if(_host == "localhost" || _host.empty()){
-        return boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), _port);
-    }else{
-        return boost::asio::ip::tcp::endpoint(boost::asio::ip::make_address(_host), _port);
+        _logger.log_file(LogLevel::INFO, "Closing connection for " + _clinet_name + " (keep_alive=false).");
+        stop();
     }
 }
 
 
+void Session::handle_request(net_string && request){
+    // boost::beast::http::response<boost::beast::http::string_body> res;
+    auto res = std::make_shared<boost::beast::http::response<boost::beast::http::string_body>>();
 
-void Server::start(){
-    try{
-        do_accept();
-        _ioc.run();
-        _logger.log_file(LogLevel::INFO, "Server started on " + _host + ":" + std::to_string(_port));
-    }catch(const std::exception & e){
-        _logger.log_file(LogLevel::ERROR, "Exception in start(): " + std::string(e.what()));
-        return;
-    }
-}
+    res->version(request.version());
+    res->keep_alive(request.keep_alive());
 
-void Server::do_accept(){
-    _acceptor.async_accept(
-        boost::asio::make_strand(_ioc),
-        [this](boost::system::error_code ec, boost::asio::ip::tcp::socket socket){
-            if(!ec){
-                auto client = std::make_shared<Session>(std::move(socket), *this);
-                _clients.emplace(client->get_client_name(), client);
-                client->start();
-            }else{
-                if(ec == boost::asio::error::operation_aborted){
-                    _logger.log_file(LogLevel::INFO, "Server acceptor stopped.");
-                    return;
-                }else{
-                    _logger.log_file(LogLevel::ERROR, "Accept error: " + ec.message());
-                }               
-            }
-        }
-    );
-}
-
-
-void Server::handle_request(
-    net_string && request,
-    boost::beast::tcp_stream & stream
-){
-
-    
-    boost::beast::http::response<boost::beast::http::string_body> res;
-
-    res.version(request.version());
-    res.keep_alive(request.keep_alive());
-
-    res.set(http_field::server,"MoneyTrail");
-    res.set(http_field::access_control_allow_origin,"*");
-    res.set(http_field::access_control_allow_methods,"POST OPTIONS");
-    res.set(http_field::access_control_allow_headers,"Content-Type");
+    res->set(http_field::server,"MoneyTrail");
+    res->set(http_field::access_control_allow_origin,"*");
+    res->set(http_field::access_control_allow_methods,"POST,OPTIONS");
+    res->set(http_field::access_control_allow_headers,"Content-Type");
 
 
     try
     {
         if(request.method() == http_method::options){
-            res.result(http_status::no_content);
-            res.body() = "";
+            res->result(http_status::no_content);
+            res->body() = "";
         }else if(request.method() == http_method::post && request.target() == "/plan"){
             nlohmann::json json_res;
             try
@@ -168,24 +119,203 @@ void Server::handle_request(
             {
                 json_res = nlohmann::json::array({{{"name","null"},{"cost",0},{"time","null"}}});
             }
-            res.result(http_status::ok);
-            res.set(http_field::content_type,"application/json");
-            res.body() = json_res.dump();
+            res->result(http_status::ok);
+            res->set(http_field::content_type,"application/json");
+            res->body() = json_res.dump();
         }else{
-            res.result(http_status::not_found);
-            res.set(http_field::content_type,"text/plain");
-            res.body() = "404 Not Found";
+            res->result(http_status::not_found);
+            res->set(http_field::content_type,"text/plain");
+            res->body() = "404 Not Found";
         }
     }
     catch(const std::exception& e)
     {
-        res.result(http_status::internal_server_error);
-        res.body() = std::string("Serer error: ") + e.what();
-        res.set(http_field::content_type, "text/plain");
+        res->result(http_status::internal_server_error);
+        res->body() = std::string("Serer error: ") + e.what();
+        res->set(http_field::content_type, "text/plain");
     }
     
 
-    res.prepare_payload();
+    res->prepare_payload();
 
-    boost::beast::http::write(stream, res);
+    _response_queue.emplace(res);
+    
+    do_write();
 }
+
+void Session::do_write(){
+    if(_closing || _writing || _response_queue.empty()){
+        return;
+    }
+
+    _writing = true;
+
+    auto response = _response_queue.front();
+
+    auto self(shared_from_this());
+
+    boost::beast::http::async_write(
+        _stream,
+        *response,
+        [self](boost::system::error_code ec, std::size_t bytes){
+            self->handle_write(ec, bytes);
+        }
+    );
+}
+
+void Session::handle_write(boost::system::error_code ec, std::size_t bytes){
+    _writing = false;
+
+    if(ec){
+        _logger.log_file(LogLevel::ERROR, "wtire error: " + ec.message());
+        return do_close();
+    }
+
+    _response_queue.pop();
+}
+
+void Session::clear_queue(){
+    //remove queue
+    while(!_response_queue.empty()){
+        _response_queue.front().reset();
+        _response_queue.pop();
+    }
+}
+
+void Session::do_close(){
+    // _server.remove_client(_clinet_name);
+
+    clear_queue();
+
+    boost::system::error_code ec;
+
+    if(!_stream.socket().is_open()){
+        _logger.log_file(LogLevel::WARNING,"Socket " + _clinet_name + " automaticlly disconnected.");
+        return;
+    }
+
+    _stream.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
+    if(ec && ec != boost::asio::error::not_connected){
+        _logger.log_file(LogLevel::WARNING, "Socket shutdown warning for " + _clinet_name +  ": " + ec.message());
+    }
+
+    _stream.socket().close(ec);
+    if(ec){
+        _logger.log_file(LogLevel::WARNING, "Socket close warning for " + _clinet_name + ": " + ec.message());
+    }
+
+    _closing.store(true);
+
+    _logger.log_file(LogLevel::INFO, "Client " + _clinet_name + " disconnected successfully.");
+}
+
+
+
+// Server
+Server::Server(std::string host, unsigned short port, int thread_number, Logger & logger)
+: _host(std::move(host)), _port(port), _thread_number(thread_number),
+  _ioc(_thread_number), _acceptor(_ioc, make_endpoint()),
+  _logger(logger)
+{
+    _logger.log_file(LogLevel::INFO, "Server initiallized Successfully.");
+}
+
+Server::~Server(){
+    stop();
+    _clients.clear();
+    _logger.log_file(LogLevel::INFO, "Server stopped. All things cleaned up.");
+}
+
+
+
+void Server::stop(){
+
+    boost::system::error_code ec;
+    _acceptor.close(ec);
+    if(ec){
+        _logger.log_file(LogLevel::ERROR, "Error closing acceptor: " + ec.message());
+    }
+    
+    close_all_clients();
+    _logger.log_file(LogLevel::WARNING, "DEBUG, ready to close ioc.");
+
+
+    //finally to stop server
+    if(!_ioc.stopped()){
+        _logger.log_file(LogLevel::WARNING, "DEBUG, ioc is stopping.");
+        _ioc.stop();
+        _logger.log_file(LogLevel::WARNING, "DEBUG, ioc has already stopped.");
+    }
+
+    _logger.log_file(LogLevel::INFO, "Server stopped successfully.");
+}
+
+void Server::add_client(const std::shared_ptr<Session> & session){
+    auto client_name = session->get_client_name();
+    _clients.emplace(client_name, session);
+    _logger.log_file(LogLevel::INFO, "Client added: " + client_name + ". Total clients: " + std::to_string(_clients.size()));
+}
+
+void Server::remove_client(const std::string & client_name){
+    if(_clients.erase(client_name) > 0){
+        _logger.log_file(LogLevel::INFO, "client removed: " + client_name + ". Remaining clients: " + std::to_string(_clients.size()));
+    }
+}
+
+void Server::close_all_clients(){
+    _logger.log_file(LogLevel::INFO, "Closing all " + std::to_string(_clients.size()) + " clients...");
+
+    for(auto & [client_name, session] : _clients){
+        if(session){
+            _logger.log_file(LogLevel::WARNING, "DEBUG," + client_name + " is stopping.");
+            session.reset();
+        }
+    }
+    // _clients.clear();
+}
+
+boost::asio::ip::tcp::endpoint Server::make_endpoint(){
+    if(_host == "localhost" || _host.empty()){
+        return boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), _port);
+    }else{
+        return boost::asio::ip::tcp::endpoint(boost::asio::ip::make_address(_host), _port);
+    }
+}
+
+
+
+void Server::start(){
+    try{
+        do_accept();
+        _logger.log_file(LogLevel::INFO, "Server started on " + _host + ":" + std::to_string(_port));
+        _ioc.run();
+    }catch(const std::exception & e){
+        _logger.log_file(LogLevel::ERROR, "Exception in start(): " + std::string(e.what()));
+        return;
+    }
+}
+
+void Server::do_accept(){
+    if(!_acceptor.is_open()) return;
+    _acceptor.async_accept(
+        boost::asio::make_strand(_ioc),
+        [this](boost::system::error_code ec, boost::asio::ip::tcp::socket socket){
+            if(!ec){
+                auto session = std::make_shared<Session>(std::move(socket), *this, _logger);
+                add_client(session);
+                session->start();
+                do_accept();
+            }else{
+                if(ec == boost::asio::error::operation_aborted){
+                    _logger.log_file(LogLevel::INFO, "Server acceptor stopped.");
+                    return;
+                }else{
+                    _logger.log_file(LogLevel::ERROR, "Accept error: " + ec.message());
+                    do_accept();
+                }               
+            }
+        }
+    );
+}
+
+
